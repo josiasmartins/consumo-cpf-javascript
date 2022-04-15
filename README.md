2697e06a4453be5f1eb3f8274a99ed836f3adfc0
Caitano <josiasmartinscaitano@gmail.com>
2022-04-15 20:50:48 -0300

    fixture: adicionado todos os métodos no js

---
+++ README.md
@@ -0,0 +1,7 @@
+# Consumindo API de CEP
+
+### DEMO
+
+### link da API de cep: viacep.com.br
+
+#### font: https://www.youtube.com/watch?v=imk6Y0viabg
--- javascript/main.js
+++ javascript/main.js
@@ -1,8 +1,42 @@
 'use strict';
 
-const pesquisarCep = () => {
+const limparFormulario = (endereco) => {
+    document.getElementById('endereco').value = '';
+    document.getElementById('bairro').value = '';
+    document.getElementById('cidade').value = '';
+    document.getElementById('estado').value = '';
+}
+
+const preencherFormulario = (endereco) => {
+    document.getElementById('endereco').value = endereco.logradouro;
+    document.getElementById('bairro').value = endereco.bairro;
+    document.getElementById('cidade').value = endereco.localidade;
+    document.getElementById('estado').value = endereco.uf;
+}
+
+const eNumero = (numero) => /^[0-9]+$/.test(numero);
+
+const cepValido = (cep) => cep.length == 8 && eNumero(cep);
+
+const pesquisarCep = async() => {
+    limparFormulario();
     const cep = document.getElementById('cep').value;
-    console.log(cep);
+    const url = `http://viacep.com.br/ws/${cep}/json/`;
+    // fetch: passa a url e ele retorna;
+    //        ele retorna um Promise
+    // fetch(url).then(response => response.json()).then(console.log);
+    if (cepValido(cep)) {
+        const dados = await fetch(url);
+        const endereco = await dados.json();
+        if (endereco.hasOwnProperty('erro')) {
+            document.getElementById('endereco').value = 'CEP não encontrado!';
+        } else {
+            preencherFormulario(endereco);
+        }
+    } else {
+        document.getElementById('endereco').value = 'CEP incorreto!';
+    }
+    console.log(endereco);
 }
 
 document.getElementById('cep')
